7049fa57747cd224fbf1adffd4b7ba8a877c6d46
Persona99 <nazarov.k.k@gmail.com>
2021-03-31 11:20:34 +0500

    couriers_get response fix

--- db/queries/get_courier.py
+++ db/queries/get_courier.py
@@ -1,4 +1,5 @@
 from sqlalchemy.sql import func
+from datetime import timedelta
 
 from db.models import DBCourier, DBOrder
 from db.database import DBSession
@@ -13,13 +14,26 @@ def get_courier_stats(session: DBSession, courier_id: int) -> dict:
     if not courier:
         raise DBDoesntExistsException
 
-    lead_time = []
+    avg_by_reg = []
 
     for reg in courier.regions:
-        lead_time.append(session.query(func.avg(DBOrder.complete_time - DBOrder.assign_time))
-                         .filter((DBOrder.region == reg) & (DBOrder.courier_id == courier_id)).first())
+        avg_by_reg.append(session.query(func.avg(DBOrder.complete_time - DBOrder.assign_time)).group_by(DBOrder.order_id)
+                         .filter((DBOrder.region == reg)
+                                 & (DBOrder.courier_id == courier_id)
+                                 & (DBOrder.complete_time != None)).all())
 
-    lead_time = [i[0] for i in lead_time if i[0] is not None]
+    completed_order: int = 0
+    lead_time: list[timedelta] = []
+
+    for time_in_reg in avg_by_reg:
+        if len(time_in_reg) > 0:
+            counter: timedelta = timedelta(0)
+            for i in time_in_reg:
+                counter += i[0]
+
+            completed_order += len(time_in_reg)
+            lead_time.append(counter / len(time_in_reg))
+
     courier_type = {v: k for k, v in courier_lifting_capacity.items()}[courier.lifting_capacity]
 
     courier_rate = {
@@ -36,10 +50,10 @@ def get_courier_stats(session: DBSession, courier_id: int) -> dict:
         'earnings': 0
     }
 
-    if len(lead_time) > 0:
+    if completed_order > 0:
         courier_data.update({
             'rating': float(f"{(60 * 60 - min(min(lead_time).total_seconds(), 60 * 60)) / (60 * 60) * 5:.2f}"),
-            'earnings': len(lead_time) * (500 * courier_rate[courier.lifting_capacity])
+            'earnings': completed_order * (500 * courier_rate[courier.lifting_capacity])
         })
 
     return courier_data
